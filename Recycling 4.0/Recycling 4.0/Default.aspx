833e1dde6d6e2ce7dd185880f27d033a13ac6bc2
Stephan <Stephan.Toennies@gmail.com>
2019-04-19 10:14:06 +0200

    clear error event
    invalid postback or callback argument

--- Recycling 4.0/Recycling 4.0/Default.aspx
+++ Recycling 4.0/Recycling 4.0/Default.aspx
@@ -1,13 +1,14 @@
-﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Recycling_4._0._Default" %>
+﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" EnableEventValidation="false" CodeBehind="Default.aspx.cs" Inherits="Recycling_4._0._Default" %>
 
 
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <div class="container">
-            <p>+++TEST Default site TEST+++</p>
+        <p>+++TEST Default site TEST+++</p>
         <form id="form1">
             <div class="centerContainer">
-                <asp:Login ID = "Login1" runat="server" Class="hidden" OnAuthenticate= "ValidateUser"></asp:Login>
+                <asp:Login ID = "Login1" runat="server" Class="hidden" OnLoggingIn= "OnLoggingIn">
+                </asp:Login>
             </div>
         </form>
     </div>
--- Recycling 4.0/Recycling 4.0/Default.aspx.cs
+++ Recycling 4.0/Recycling 4.0/Default.aspx.cs
@@ -20,17 +20,19 @@ namespace Recycling_4._0
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            Login login = new Login();
-            login.ID = "Login1";
-            Login1.Controls.Add(login);
+            
+                Login login = new Login();
+                login.ID = "Login1";
+                Login1.Controls.Add(login);
+            
+            
 
             
 
         }
 
-        protected void ValidateUser(object sender, EventArgs e)
+        protected void OnLoggingIn(object sender, EventArgs e)
         {
-            Login Login1 = (Login) sender;
 
             if (Login1.UserName.Equals(user1.getName()) && Login1.Password.Equals(user1.getPassword()) || 
                 Login1.UserName.Equals(user2.getName()) && Login1.Password.Equals(user2.getPassword()))
@@ -45,39 +47,6 @@ namespace Recycling_4._0
                 Login1.InstructionText = "Der Nutzername oder E-Mail ist falsch.";
             }
 
-            
-
-            /*
-            int userId = 0;
-            string constr = ConfigurationManager.ConnectionStrings["constr"].ConnectionString;
-            using (SqlConnection con = new SqlConnection(constr))
-            {
-                using (SqlCommand cmd = new SqlCommand("Validate_User"))
-                {
-                    cmd.CommandType = CommandType.StoredProcedure;
-                    cmd.Parameters.AddWithValue("@Username", Login1.UserName);
-                    cmd.Parameters.AddWithValue("@Password", Login1.Password);
-                    cmd.Connection = con;
-                    con.Open();
-                    userId = Convert.ToInt32(cmd.ExecuteScalar());
-                    con.Close();
-                }
-                switch (userId)
-                {
-                    //testen
-                    case -1:
-                        Login1.FailureText = "Username and/or password is incorrect.";
-                        break;
-                    case -2:
-                        Login1.FailureText = "Account has not been activated.";
-                        break;
-                    default:
-                        FormsAuthentication.RedirectFromLoginPage(Login1.UserName, Login1.RememberMeSet);
-                        break;
-                }
-            
-            }
-            */
         }
     }
 }

ebe3939fab6e2ce32f09580be30eb73cb961f7a2
Stephan <Stephan.Toennies@gmail.com>
2019-04-20 16:39:31 +0200

    add data structure for uploaded xmls

---
+++ Recycling 4.0/Recycling 4.0/UploadingXML.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Recycling_4._0
+{
+    public class UploadingXML
+    {
+        public LocalDataStoreSlot xmlData;
+        public DateTime dateOfUploade;
+        public User uploadedUser;
+        public int costForTheXML;
+
+        public UploadingXML(Object xml, User user, int xmlCost)
+        {
+            xmlData = (LocalDataStoreSlot) xml;
+            dateOfUploade = DateTime.Now;
+            uploadedUser = user;
+            costForTheXML = xmlCost;
+        }
+    }
+}

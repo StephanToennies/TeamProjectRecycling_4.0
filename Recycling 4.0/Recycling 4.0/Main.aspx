6ef09a47f099fe4fec597a0212e90ad6df9175ac
Stephan <Stephan.Toennies@gmail.com>
2019-04-22 13:06:31 +0200

    fix design of main page

--- Recycling 4.0/Recycling 4.0/Main.aspx
+++ Recycling 4.0/Recycling 4.0/Main.aspx
@@ -3,7 +3,7 @@
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
     <h1>Laden Sie ihre XML Datei hoch</h1>
     <asp:Label ID="Label1" runat="server" Text="Hochzuladnede Datei:"></asp:Label>
-    <asp:FileUpload ID="FileUpload1" runat="server"/>
+    <asp:FileUpload ID="FileUpload1" runat="server" Class="hidden"/>
     <asp:Label ID="Label2" runat="server" Text="Festgelegter Preis für den Verkauf"></asp:Label>
     <asp:TextBox ID="TextBox1" runat="server" Width="80px"></asp:TextBox>
     <asp:Button ID="Button1" runat="server" Text="Hochladen" OnClick="addNewXML"/>
--- Recycling 4.0/Recycling 4.0/Main.aspx.cs
+++ Recycling 4.0/Recycling 4.0/Main.aspx.cs
@@ -16,34 +16,45 @@ namespace Recycling_4._0
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            
             if (!IsCallback)
             {
+            
                 //inittialisiere Label
                 Label label1 = new Label();
                 label1.ID = "Label1";
+                label1.Text = "Hochzuladnede Datei:";
+                label1.Visible = true;
                 Label1.Controls.Add(label1);
 
                 //inittialisiere Fileuploade
                 FileUpload fileupload = new FileUpload();
                 fileupload.ID = "FileUpload1";
+                fileupload.Visible = true;
                 FileUpload1.Controls.Add(fileupload);
 
                 //inittialisiere Label
                 Label label2 = new Label();
                 label2.ID = "Label2";
+                label2.Text = "Festgelegter Preis für den Verkauf";
+                label2.Visible = true;
                 Label2.Controls.Add(label2);
 
                 //intitalise TextBox 
                 TextBox textbox = new TextBox();
                 textbox.ID = "TextBox1";
+                textbox.Visible = true;
                 TextBox1.Controls.Add(textbox);
                 
 
                 //intitalise Button 
                 Button button = new Button();
                 button.ID = "Button1";
+                button.Visible = true;
                 Button1.Controls.Add(button);
+                
             }
+            
             //TODO
             
             //Ruft Wert von der Textbox auf und wandelt diese in in int um

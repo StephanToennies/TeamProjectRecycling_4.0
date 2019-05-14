8d7e8a6b7df5be9c364ec273b54bc2e531d94b68
Stephan <Stephan.Toennies@gmail.com>
2019-05-14 11:01:16 +0200

    add new project and configuration

---
+++ WebSite1/WebSite1/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script src="App_Data/Scripts/jquery-3.3.1.js"></script>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+        </p>
+        <p>
+            <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="Welcome.aspx">HyperLink</asp:HyperLink>
+        </p>
+        <asp:Login ID="Login1" runat="server" DestinationPageUrl="~/Welcome.aspx" OnAuthenticate="Login1_Authenticate">
+        </asp:Login>
+    </form>
+</body>
+</html>
---
+++ WebSite1/WebSite1/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+
+    private string strconnct = WebConfigurationManager.ConnectionStrings["DatabaseConnectionString1"].ConnectionString;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    private bool UserLogin(string un, string pw)
+    {
+        SqlConnection con = new SqlConnection(strconnct);
+        SqlCommand cmd = new SqlCommand("Select username from users where username=@un and password=@pw", con);
+        cmd.Parameters.AddWithValue("@un",un);
+        cmd.Parameters.AddWithValue("@pw", pw);
+        con.Open();
+        String result = Convert.ToString(cmd.ExecuteScalarAsync());
+
+        if (String.IsNullOrEmpty(result)) return false; return true;
+
+
+    }
+
+    protected void Login1_Authenticate(object sender, AuthenticateEventArgs e)
+    {
+        string un = Login1.UserName;
+        string pw = Login1.Password;
+
+        bool result = UserLogin(un,pw);
+
+        if (result)
+        {
+            e.Authenticated = true;
+            Session["username"] = un;
+
+        }
+        else e.Authenticated = false;
+
+
+    }
+
+
+    protected void TextBox1_TextChanged(object sender, EventArgs e)
+    {
+        Session["username"] = TextBox1.Text;
+    }
+}
